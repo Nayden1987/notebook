5a8f46791e98e6410dde00af80f20dadcc97ed3b
Nayden Dimitrov <nayden.dimitrov1987@gmail.com>
2019-10-16 19:56:01 +0300

    Update controller.php

--- controller.php
+++ controller.php
@@ -14,22 +14,6 @@ if (isset($_POST["submit"])) {
 
 function showAllNotes(){
     $notes = [];
-   //$note["subject"] = $_POST["subject"];
     $notes = getAllNotes();
     return $notes;
 }
-
-
-//$new_note = changeNote($id, $_POST["note"]);
-//$result["new_note"] = $new_note;
-//echo json_encode($result);
-//
-//
-//if(isset($_POST["delete"])){
-//    if(delete($id)){
-//        header("Location: main.php");
-//    }
-//    else{
-//        echo "Error";
-//    }
-//}
